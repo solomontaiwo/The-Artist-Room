6a46318890fb36defd8cb16d28f8ddfb220c48b9
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-12-25 15:04:51 +0100

    Aggiunto 'available_seats' come attributo dello schema 'rooms'

--- database/migrations/2023_12_25_140142_create_rooms_table.php
+++ database/migrations/2023_12_25_140142_create_rooms_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->integer('size');
+            $table->integer('available_seats'); // posti disponibili, valore decrementato ad ogni prenotazione
             $table->timestamps();
         });
     }
