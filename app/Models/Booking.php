ede544a515ed11b5e94ed68739a4354a3eae52b6
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-12-29 21:03:11 +0100

    Eliminazione funzione getMaxDeparture in Modello Booking e aggiunto Modelli a info progetto

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -23,15 +23,6 @@ class Booking extends Model
         'people'
     ];
 
-    public function getMaxDeparture($arrival_date)
-    {
-        $numberOfDaysToAdd = 2;
-
-        $arrival_date->modify('+' . $numberOfDaysToAdd . ' days');
-
-        return $arrival_date;
-    }
-
     // Relazione tra prenotazioni e utente
     public function user()
     {
