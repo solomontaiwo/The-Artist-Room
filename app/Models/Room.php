80edec7bc5e15314fe0fbc0f6a685c5a803d7e55
giorgiapirelli <151879700+giorgiapirelli@users.noreply.github.com>
2024-01-06 12:03:12 +0100

    solo commenti

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -20,4 +20,9 @@ class Room extends Model
     {
         return $this->hasMany(Booking::class);
     }
+    /*
+    return $this->hasMany(Booking::class);: Questa riga di codice utilizza il metodo hasMany per definire una relazione uno-a-molti. 
+    In questo caso, indica che l'istanza corrente del modello ha molteplici record associati nel modello Booking. Il parametro Booking::class 
+    specifica il modello a cui è associata la relazione.
+    */
 }
