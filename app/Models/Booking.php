c84a7157286b5e4505ef837f11c92cebdb3ce9e9
giorgiapirelli <151879700+giorgiapirelli@users.noreply.github.com>
2024-01-06 11:58:08 +0100

    Update Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -29,6 +29,9 @@ class Booking extends Model
     }
 
     // Relazione tra prenotazioni e stanza
+    //Questo codice viene tipicamente trovato all'interno di una classe di modello Eloquent di Laravel e stabilisce una relazione
+    //tra le istanze del modello corrente e le istanze del modello Room. Questa relazione si basa spesso sulla presenza di una chiave 
+    //esterna nella tabella del database del modello corrente che fa riferimento alla chiave primaria della tabella del modello Room.
     public function room()
     {
         return $this->belongsTo(Room::class);
